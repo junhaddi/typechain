f95ac5a41fb78e6fd3802080db3159f175df594d
junhaddi <rkdwnsgk05@gmail.com>
2020-12-25 16:59:27 +0900

    TS Class 공부

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: 'junha',
-  age: 19,
-  gender: 'male',
-};
+const junha = new Human('junha', 19, 'male');
 
 const sayHi = (person: Human): string => {
   return `name: ${person.name}, age: ${person.age}, gender: ${person.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(junha));
 
 export {};
