93085cdb1936264084b52ab83b29f5f28d5a2c01
junhaddi <rkdwnsgk05@gmail.com>
2020-12-25 17:47:26 +0900

    블록체인 생성 구현

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
+import * as CryptoJS from 'crypto-js';
+import { create } from 'domain';
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
   constructor(
     index: number,
     hash: string,
@@ -21,8 +33,34 @@ class Block {
 
 const genesisBlock: Block = new Block(0, '202020202020', '', 'Hello', 123456);
 
-let blockChain: [Block] = [genesisBlock];
+let blockChain: Block[] = [genesisBlock];
 
-console.log(blockChain);
+const getBlockChain = (): Block[] => blockChain;
+
+const getLatestBlock = (): Block => blockChain[blockChain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimeStamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimeStamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimeStamp
+  );
+  return newBlock;
+};
+
+console.log(createNewBlock('Block1'), createNewBlock('Block2'));
 
 export {};
